2d24b5ec53690d12a454c713cfd3719e6e49745f
venkat.veeramanidaas <Venkat.Veeramanidaasan@demandmedia.com>
2015-01-30 13:32:17 +0530

    WIP - to include staging module simulation

--- daq/__main__.py
+++ daq/__main__.py
@@ -22,20 +22,7 @@ def set_logger():
     log = logging.getLogger(__name__)
 
 
-def main(argv=None):
-    set_logger()
-    argv = argv or sys.argv
-    parser = ArgumentParser(description="Daq tool")
-
-    parser.add_argument('--date',
-                        help='event date - in YYYY-MM-DD format - PST/PDT',
-                        dest='date',
-                        type=date_type,
-                        required=True)
-
-    args = parser.parse_args(argv[1:])
-    log.info('Args: {}'.format(args))
-
+def daq_helper(args):
     # Simulation
     date = args.date.strftime(DATE_FORMAT)
     if (date == "2015-02-02"):
@@ -44,8 +31,45 @@ def main(argv=None):
     elif (date == "2015-02-03"):
         log.info('Unhandled exception - scenario')
         i = 1/0
-
     return 0
+
+
+def daq_subparser(sub_parsers):
+    args = sub_parsers.add_parser("daq", "DAQ execution")
+    args.add_argument('--date',
+                     help='event date - in YYYY-MM-DD format - PST/PDT',
+                     dest='date',
+                     type=date_type,
+                     required=True)
+    args.set_defaults(func=daq_helper)
+
+
+def staging_helper(args):
+    log.info('Batch Id {}'.format(args.batch_id))
+
+
+def staging_subparser(sub_parsers):
+    args = sub_parsers.add_parser("staging", "Staging execution")
+    args.add_argument('--batch-id',
+                     help='Batch Id from the DP',
+                     dest='batch_id',
+                     type=int,
+                     required=True)
+    args.set_defaults(func=staging_helper)
+
+
+def main(argv=None):
+    set_logger()
+    argv = argv or sys.argv
+    parser = ArgumentParser(description="Daq tool")
+    subparsers = parser.add_subparsers()
+
+    daq_subparser(subparsers)
+    staging_subparser(subparsers)
+
+    args = parser.parse_args(argv[1:])
+    log.info('Args: {}'.format(args))
+    return args.func(args)
 
 
 if __name__ == '__main__':
